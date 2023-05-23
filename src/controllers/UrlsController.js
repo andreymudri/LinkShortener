3be78ff8f971ae9294bff934078de1599fb19f94
Andrey Mudri <andreybeckert@gmail.com>
2023-05-23 00:55:46 -0300

    avaliador fix

--- src/controllers/UrlsController.js
+++ src/controllers/UrlsController.js
@@ -28,7 +28,10 @@ export async function postUrlShortener(req, res) {
 export async function getUrlbyID(req, res) {
   try {
     const { id } = req.params;
-    const result = await db.query(`SELECT id, link as url, short_link as "shortUrl", views FROM urls WHERE id = $1;`, [id]);
+    const result = await db.query(
+      `SELECT id, link as url, short_link as "shortUrl", views FROM urls WHERE id = $1;`,
+      [id]
+    );
 
     if (result.rows.length === 0) {
       return res.status(404).send({ error: "URL not found" });
@@ -61,25 +64,27 @@ export async function getShortUrl(req, res) {
   }
 }
 
-
 export async function deleteUrl(req, res) {
   const urlId = req.params.id;
 
   try {
-    const result = await db.query(`SELECT user_email FROM urls WHERE id = $1`, [urlId]);
+    const result = await db.query(
+      `SELECT user_email FROM urls WHERE id = $1;`,
+      [urlId]
+    );
 
     if (result.rowCount === 0) {
-      return res.status(404).json({ error: 'URL não encontrada.' });
+      return res.status(404).json({ error: "URL não encontrada." });
     }
     const user_email = result.rows[0].user_email;
     if (user_email !== req.user.email) {
-      return res.status(401).json({ error: 'Acesso não autorizado.' });
+      return res.status(401).json({ error: "Acesso não autorizado." });
     }
-    await db.query(`DELETE FROM urls WHERE id = $1`, [urlId]);
+    await db.query(`DELETE FROM urls WHERE id = $1;`, [urlId]);
 
-    return res.status(204);
+    return res.sendStatus(204);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Erro interno do servidor.' });
+    return res.status(500).json({ error: "Erro interno do servidor." });
   }
-};
+}
